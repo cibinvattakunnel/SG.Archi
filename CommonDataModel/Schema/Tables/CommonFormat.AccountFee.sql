59f7adde08782ef867277b6b11600997f423320f
Cibin <Cibin.Vattakunnel@smartsalary.com.au>
2018-05-02 14:03:44 +1000

    Added more fee related columns

--- CommonDataModel/Schema/Tables/CommonFormat.AccountFee.sql
+++ CommonDataModel/Schema/Tables/CommonFormat.AccountFee.sql
@@ -9,6 +9,9 @@ CREATE TABLE [CommonFormat].[AccountFee]
 [FeeAmountPerPay] [decimal] (16, 2) NULL,
 [FeeAmountPerAnnum] [decimal] (16, 2) NULL,
 [FeeAmountPercent] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PaymentMethod] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreditorId] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[InvoiceFormat] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [LastUpdatedTimestamp] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [JSON] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
@@ -23,6 +26,8 @@ EXEC sp_addextendedproperty N'MS_Description', N'Source system ID for employer/c
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Source system ID Account Invoice Cycle ID', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'AccountInvoiceCycleId'
 GO
+EXEC sp_addextendedproperty N'MS_Description', N'Creditor used ', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'CreditorId'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Fee amount per annum', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'FeeAmountPerAnnum'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Fee amount percent', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'FeeAmountPercent'
@@ -35,7 +40,11 @@ EXEC sp_addextendedproperty N'MS_Description', N' Fee name /Sample Data : Admi
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Fee type /Sample Data:Admin Fee,HA Fee,WP Fee,Other Fee', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'FeeType'
 GO
+EXEC sp_addextendedproperty N'MS_Description', N'remittance format /Sample data : PDF ,Excel ', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'InvoiceFormat'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Any additional values to populate /Sample data :{"name":"value"}', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'JSON'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Last date time the contact record has been updated /Sample Data :', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'LastUpdatedTimestamp'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Payment method /Sample Data :Employer pays In GST ', 'SCHEMA', N'CommonFormat', 'TABLE', N'AccountFee', 'COLUMN', N'PaymentMethod'
 GO
