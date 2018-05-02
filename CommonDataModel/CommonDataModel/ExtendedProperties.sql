3406d49028f006f7a1be2e568c896cfaf8f1bb20
Cibin <Cibin.Vattakunnel@smartsalary.com.au>
2018-05-02 16:53:24 +1000

    clean up files

--- CommonDataModel/CommonDataModel/ExtendedProperties.sql
+++
@@ -1,2 +0,0 @@
-﻿EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Common data model for smartgroup data migration ';
-
--- CommonDataModel/CommonDataModel/Security/CommonFormat.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE SCHEMA [CommonFormat]
-    AUTHORIZATION [dbo];
-
-
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'common format schema', @level0type = N'SCHEMA', @level0name = N'CommonFormat';
-
